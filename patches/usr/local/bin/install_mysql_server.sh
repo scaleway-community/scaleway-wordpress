ac74ad2b6ff116edf7ae4e2cbdd3c17cc48cb9ea
Edouard Bonlieu <ebonlieusubs@gmail.com>
2014-12-30 17:37:41 +0100

    Update install_mysql_server.sh

--- patches/usr/local/bin/install_mysql_server.sh
+++ patches/usr/local/bin/install_mysql_server.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
 if [ ! -f /root/.my.cnf ]; then
+
     password=$(pwgen 42)
     dbname="wordpress"
     username="root"
+    
+    /usr/bin/mysqladmin -u root password $password
     cat <<EOF > /root/.my.cnf
 [client]
 user = $username
 password = $password
 EOF
 
-    # Reset mysql root password
-    cat <<EOF > /tmp/recover_root_mysql.ini
-UPDATE mysql.user SET password=PASSWORD("$password") where User='$username';
-FLUSH PRIVILEGES;
-EOF
-    service mysql stop
-    mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
+    mysql -e "CREATE DATABASE $dbname"
 
-    while ! [[ "$mysqld_process_pid" =~ ^[0-9]+$ ]]; do
-	    mysqld_process_pid=$(echo "$(ps -C mysqld -o pid=)" | sed -e 's/^ *//g' -e 's/ *$//g')
-	    sleep 1
-    done
-
-    echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
-
-    rm -f /tmp/recover_root_mysql.ini
-    killall mysqld
-    sleep 5
-    killall -9 mysqld
-
-    service mysql start
-    
     # Configure wordpress database
     sed -i "s/define('DB_NAME',.*/define('DB_NAME', '$dbname');/" /var/www/wp-config.php
     sed -i "s/define('DB_USER',.*/define('DB_USER', '$username');/" /var/www/wp-config.php
