4c19b234f0914eed3c779c104904029c965ead2a
Manfred Touron <m@42.am>
2015-02-09 15:54:00 +0000

    wp-config.php: Added FS_METHOD=direct (Relates #1) (Fixes #4)

--- patches/usr/local/bin/install_mysql_server.sh
+++ patches/usr/local/bin/install_mysql_server.sh
@@ -22,4 +22,5 @@ EOF
     sed -i "s/define('DB_NAME',.*/define('DB_NAME', '$dbname');/" /var/www/wp-config.php
     sed -i "s/define('DB_USER',.*/define('DB_USER', '$username');/" /var/www/wp-config.php
     sed -i "s/define('DB_PASSWORD',.*/define('DB_PASSWORD', '$password');/" /var/www/wp-config.php
+    sed -i "/WP_DEBUG/a\\define('FS_METHOD', 'direct');" /var/www/wp-config.php
 fi
