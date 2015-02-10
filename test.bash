53c2bf1fab94ff32822d76406cfcd8ab34ea85cb
Manfred Touron <m@42.am>
2015-02-10 23:44:13 +0100

    Create test.bash

---
+++ test.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+which shunit2 >/dev/null || (apt-get -qq update; apt-get install -yq shunit2)
+
+testMysqlConnect() {
+        echo SELECT 1 | mysql >/dev/null
+        returnCode=$?
+        assertEquals "Cannot connect to mysql without parameters" 0 $returnCode
+}
+
+testMysqlDBExists() {
+        echo SELECT 1 | mysql wordpress >/dev/null
+        returnCode=$?
+        assertEquals "Cannot connect to wordpress database" 0 $returnCode
+}
+
+testMysqlUserExists() {
+        echo 'select count(*) from wp_users' | mysql wordpress | grep 1 >/dev/null
+        returnCode=$?
+        assertEquals "No admin account" 0 $returnCode
+}
+
+. shunit2
