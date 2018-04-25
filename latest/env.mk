cba72b50426f52f4a362a7b7ef37ee509513b1fc
Maël Le Garrec <mlegarrec@online.net>
2018-04-25 13:17:05 +0000

    convert to new build system

---
+++ latest/env.mk
@@ -0,0 +1,7 @@
+IMAGE_NAME = wordpress
+IMAGE_VERSION = latest
+IMAGE_VERSION_ALIASES =	4.8.0 4.8 4
+IMAGE_TITLE = WordPress
+IMAGE_DESCRIPTION = WordPress with MySQL
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-wordpress
+IMAGE_BOOTSCRIPT = mainline 4.9
